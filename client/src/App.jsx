29459a163cf36c37cf17ef9b71311193c612f84a
hyein310 <kanghyein310@naver.com>
2025-05-13 18:15:53 +0900

    client 단 추가 및 수정 완료

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Intro from './pages/Intro';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/old_Dashboard';
+import NewDashboard from './pages/NewDashboard';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Intro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<NewDashboard />} />
+            <Route path="/old-dashboard" element={<Dashboard />} />
+            {/* 추가적인 라우트는 여기에 추가될 예정 */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  const title = "Meetingly";
-
-  return (
-    <div>
-      <h1>우리 프로젝트의 이름은 {title}</h1>
-    </div>
-  );
-}
