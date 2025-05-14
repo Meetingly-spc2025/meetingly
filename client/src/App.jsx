e989cf1f9bcdb4946f5201e95cafc42a6b75df2b
hyein310 <kanghyein310@naver.com>
2025-05-14 17:55:06 +0900

    [docs] 디렉토리 구조 cra -> vite 변경 완료.. 추후 server단 express 부분 수정 필요.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Header/Navbar'
+import Navbar from './components/Navbar';
 import Intro from './pages/Intro';
 import Login from './pages/Login';
 import Register from './pages/Register';
