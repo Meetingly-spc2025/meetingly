4d95b7117319b40b960846cf51c0948ce47150a2
hyein310 <kanghyein310@naver.com>
2025-05-14 16:17:00 +0900

    client단 local에서 돌아가도록 함(vite 부분 cra 사용하도록 수정)

--- client/src/pages/NewDashboard.jsx
+++ client/src/pages/NewDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/Header/Sidebar';
 import { FaEllipsisH, FaPlus } from 'react-icons/fa';
 import '../styles/NewDashboard.css';
 
