7909dffd541deac6d5827c14278833846fe50093
hyein310 <kanghyein310@naver.com>
2025-05-15 17:21:02 +0900

    [docs] 디렉토리 구조 변경 완료, server의 mvc 부분 더 보강해서 업데이트 예정

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const path = require("path");
 
-const app = express();
-const PORT = 8080;
+const apiRouter = require("./routes/apiRouter");
 
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+// 라우터
+app.use("/api", apiRouter);
+
+// 배포 모드
+if (process.env.NODE_ENV === "production") {
+  const rootPath = path.resolve(__dirname, "../../client");
+  app.use(express.static(rootPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(rootPath, "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("server is open!");
-    console.log("http://localhost:" + PORT);
-})
+  console.log(` Server running on port ${PORT}`);
+});
