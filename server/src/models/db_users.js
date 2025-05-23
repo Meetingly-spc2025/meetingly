6ec4d38146bd344919d0cee78e0a8fcb53b8c08e
hyein310 <kanghyein310@naver.com>
2025-05-23 18:42:16 +0900

    [Fix] merge후 코드 오류 수정

--- server/src/models/db_users.js
+++ server/src/models/db_users.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+require("dotenv").config({ path: "../.env" });
 
 // require('dotenv')
 
-    // { 
-    //   config: [Function: config],
-    //   parse: [Function: parse]
-    // }
+// {
+//   config: [Function: config],
+//   parse: [Function: parse]
+// }
 
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
 });
 
 module.exports = pool;
-// DB 
+// DB
