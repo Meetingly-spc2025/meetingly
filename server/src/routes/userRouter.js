8e88348b26f071f1357b1be40c5bc9a1f4c81f59
surferofsurfer <surfersurferlife93@gmail.com>
2025-05-25 17:58:27 +0900

    Feat:토큰 보내면 로그인된 사용자 맞는지 여부 검증 후 로그인된 사용자의 정보를 반환하는 /api/users/jwtauth API 구현

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,24 +1,16 @@
 const express = require("express");
-// Node.js 에서 Express 라이브러리를 가져온다
-
 const router = express.Router();
-
-// express 안에 .Router() 라는 라우팅 전용 객체 (함수)가 들어있음.
 
 // 로그인 로직 import
 const { loginUser } = require("../controllers/userController");
 
+// jwt 토큰 검증 미들웨어 로직 import. 아래가 next()
+const { authenticate } = require("../middlewares/authJwtMiddleware");
+// jwt 토큰 정상 시 유저 정보 주는 로직
+const { getUserInfo } = require("../controllers/userController");
+
 // 실제 라우터 등록 - 로그인
 router.post("/login", loginUser);
-
-// // 실제 라우터 등록 - 회원가입 예정
-// router.post("/register", );
-
-// 12번이 실행되자마자 POST / login 이라는 라우트 정보가 내부적으로 저장됨
-// router = {
-//   stack: [
-//     { path: '/login', method: 'POST', handler: loginUser }
-//   ]
-// }
+router.get("/jwtauth", authenticate, getUserInfo);
 
 module.exports = router;
