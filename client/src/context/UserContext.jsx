aeee02acdcd609545362a012ea5d5d24e9a55d3e
surferofsurfer <surfersurferlife93@gmail.com>
2025-06-08 13:48:36 +0900

    [Refactor]:회원가입 부분에서 실시간으로 유효성 검사하는 코드 깔끔하게 정리

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, useState, useEffect } from "react";
 
 // 1. Context 생성
 const userContext = createContext();
@@ -6,6 +7,31 @@ const userContext = createContext();
 // 2. Provider 컴포넌트 정의
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+
+    useEffect(()=> {
+        const fetchUser = async () => {
+            // 토큰 꺼내서
+            const token = localStorage.getItem("token");
+            // 토큰 없으면 일단 종료
+            if (!token) return;
+
+            try {
+                const res = await axios.get("/api/users/jwtauth", {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                });
+                setUser(res.data.user);
+                console.log("토큰 로그인 복원 디버깅", res.data.user);
+
+            } catch (error) {
+                console.error("토큰 인증 실패")
+                localStorage.removeItem("token")
+            }
+        };
+        fetchUser();
+    }, []);
+
     return (
         <userContext.Provider value={{user, setUser}}>
             {children}
